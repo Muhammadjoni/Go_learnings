e08332b7ba69da4b8bc9e7d6f6aeebad65c60a7b
Muhammadjoni <mrahimzod@gmail.com>
2022-12-20 11:06:45 +0400

    some silly validations added

--- solo_practico/entity/video.go
+++ solo_practico/entity/video.go
@@ -1,7 +1,13 @@
 package entity
 
+type Body struct {
+	Name  string `json:"name" binding:"required"`
+	Age   int    `json:"age" binding:"gte=1, lte=22"`
+	Email string `json:"email" validate:"required,email"`
+}
+
 type Video struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	URL         string `json:"url"`
+	Title       string `json:"title" binding:"min=2,max=10"`
+	Description string `json:"description" binding:"max=15"`
+	URL         string `json:"url" binding:"required,url"`
 }
