90faee5651f9506514da52f25f5a5eaed5ce87d0
Muhammadjoni <mrahimzod@gmail.com>
2023-02-02 14:43:35 +0400

    persisting user data, experimenting

---
+++ grpc_prtc/usermgmt_client/usermgmt_client.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	pb "grpc_prtc/usermgmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	port = ":30000"
+)
+
+func main() {
+	conn, err := grpc.Dial(port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewUserManagementClient(conn)
+
+	//Contact the server & get the response
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var new_users = make(map[string]int32)
+	new_users["Alice"] = 43
+	new_users["Bob"] = 31
+
+	for name, age := range new_users {
+		r, err := c.CreateUser(ctx, &pb.NewUser{Name: name, Age: age})
+		if err != nil {
+			log.Fatalf("could not create a user: %v", err)
+		}
+		log.Printf(`Created user: ID: %d NAME: %s AGE: %d`, r.GetId(), r.GetName(), r.GetAge())
+
+	}
+	params := &pb.GetUsersParams{}
+	r, err := c.GetUsers(ctx, params)
+	if err != nil {
+		log.Fatalf("could not retrieve users: %v", err)
+	}
+
+	log.Print("\nUSER LIST: \n")
+	fmt.Printf("aaaaaaaaaaaaaaaaaand the r.GetUsers() have this data: %v\n", r.GetUsers())
+}
--- grpc_prtc/usermgmt_server/usermgmt_server.go
+++ grpc_prtc/usermgmt_server/usermgmt_server.go
@@ -2,19 +2,59 @@ package main
 
 import (
 	"context"
-	pb "grpc_prtc/usermgmt"
+
 	"log"
+	"math/rand"
+	"net"
+
+	pb "grpc_prtc/usermgmt"
+
+	"google.golang.org/grpc"
 )
 
 var (
 	port = ":30000"
 )
 
-type UserManagementServer struct {
-	pb.UnimplementedUserManagementServer
+func NewUserManagementServer() *UserManagementServer {
+	return &UserManagementServer{
+		user_list: &pb.Userlist{},
+	}
 }
 
-func (s *UserManagementServer) CreateUser(ctx context.Context, in pb.NewUser) {
+type UserManagementServer struct {
+	pb.UnimplementedUserManagementServer
+	user_list *pb.Userlist
+}
+
+func (server *UserManagementServer) Run() error {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterUserManagementServer(s, server)
+	log.Printf("server listening at %v", lis.Addr())
+	return s.Serve(lis)
+}
+
+func (s *UserManagementServer) CreateUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
 	log.Printf("Received: %v", in.GetName())
-	return &pb.NewUser{Message: } 
+	var user_id int32 = int32(rand.Intn(100))
+	created_user := &pb.User{Id: user_id, Name: in.GetName(), Age: in.GetAge()}
+	s.user_list.Users = append(s.user_list.Users, created_user)
+
+	return created_user, nil
+}
+
+func (s *UserManagementServer) GetUsers(ctx context.Context, in *pb.GetUsersParams) (*pb.Userlist, error) {
+	return s.user_list, nil
+}
+
+func main() {
+	var user_mgmt_server *UserManagementServer = NewUserManagementServer()
+	if err := user_mgmt_server.Run(); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
 }
